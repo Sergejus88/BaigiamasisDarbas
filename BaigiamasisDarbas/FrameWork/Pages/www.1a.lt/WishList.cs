f45186f78d66069491e768b11b17f47d77b1bc12
Sergejus88 <Sergejus@DESKTOP-VE3RPFL>
2023-02-25 22:59:28 +0200

    Updates Tests

---
+++ BaigiamasisDarbas/FrameWork/Pages/www.1a.lt/WishList.cs
@@ -0,0 +1,21 @@
+﻿using FrameWork.Pages;
+
+namespace FrameWork
+{
+    public class WishList
+    {
+        public static void Open()
+        {
+            Driver.OpenUrl("https://www.1a.lt/wishlists");
+        }
+        public static void ClickOnItems(string clickOnItems)
+        {
+            Common.ClickElement(Locators.WishList.clickbutton, clickOnItems);
+        }
+
+        public static void ClickLikeButton(string wishItemListPlace)
+        {
+            Common.ClickElement(Locators.WishList.clickOnWishlist, wishItemListPlace);
+        }
+    }
+}
---
+++ BaigiamasisDarbas/Tests/Pages/www.1a.ltTests/WishLisTest.cs
@@ -0,0 +1,32 @@
+﻿using FrameWork;
+using NUnit.Framework;
+using Tests.Pages.BaseTests;
+
+namespace Tests 
+{
+    internal class WishListTest : BaseTest
+    {   
+        [SetUp]
+        public void Open()
+        {
+            WishList.Open();
+        }
+
+        [Test]
+
+        public void AddSomeProductToWishlist()
+        {
+            string expectedResult = ("Added items in wishlist");
+            string actualResult = ("Added items in wishlist");
+
+            string ClickOnItems = "ClickOnItems";
+            string ClickLikeButton = "ClickLikeButton";
+                
+            WishList.ClickOnItems(ClickOnItems);
+            WishList.ClickLikeButton(ClickLikeButton);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        } 
+    }
+}
+
